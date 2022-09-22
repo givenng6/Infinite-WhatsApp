4c770ecb60d4c8d636291a66c7b04b2b8f321720
Given NG Mathebula <80547520+givenng6@users.noreply.github.com>
2022-09-22 23:09:22 +0200

    Update

--- messenger.py
+++ messenger.py
@@ -3,7 +3,7 @@ import pywhatkit
 from datetime import datetime
 
 
-receiver = "";
+receiver = ""; # cell number with country code...
 message = ""
 
 def getTime():
@@ -15,12 +15,14 @@ def getTime():
     return hours, mins
 
 sent = 0
+
 while(True):
+    # can use time to schedule messages
     time = getTime()
     hours = int(time[0])
     mins = int(time[1]) 
-    if sent == 0:
-        mins+= 1
+    
     pywhatkit.sendwhatmsg_instantly(receiver, message, 12, tab_close=True)
     sent+= 1
+
     print(sent)
